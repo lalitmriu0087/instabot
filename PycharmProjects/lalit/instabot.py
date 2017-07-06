1bcc8e484bfd12b24d2ac9c42f9ca8cc0603b232
lalitmriu0087 <lalitmriu0087@gmail.com>
2017-07-06 09:47:54 -0700

    image add

--- PycharmProjects/lalit/instabot.py
+++ PycharmProjects/lalit/instabot.py
@@ -1,4 +1,4 @@
-import requests
+import requests , urllib
 
 APP_ACCESS_TOKEN = '1394905246.c4413d6.bba3274e316c46f7a7faca24379619bb'
 #Token Owner : AVinstaBot.main
@@ -66,6 +66,31 @@ def get_user_info(insta_username):
             print 'There is no data for this user!'
     else:
         print 'Status code other than 200 received!'
+
+        '''
+        Function declaration to get the recent post of a user by username
+        '''
+
+        def get_user_post(insta_username):
+            user_id = get_user_id(insta_username)
+            if user_id == None:
+                print 'User does not exist!'
+                exit()
+            request_url = (BASE_URL + 'users/%s/media/recent/?access_token=%s') % (user_id, APP_ACCESS_TOKEN)
+            print 'GET request url : %s' % (request_url)
+            user_media = requests.get(request_url).json()
+
+            if user_media['meta']['code'] == 200:
+                if len(user_media['data']):
+                    image_name = user_media['data'][0]['id'] + '.jpeg'
+                    image_url = user_media['data'][0]['images']['standard_resolution']['url']
+                    urllib.urlretrieve(image_url, image_name)
+                    print 'Your image has been downloaded!'
+                else:
+                    print 'Post does not exist!'
+            else:
+                print 'Status code other than 200 received!'
+
 
 def start_bot():
     while True:
